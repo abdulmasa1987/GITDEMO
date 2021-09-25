5fcfd5a835be6911352de90640bd8ae2486975e9
Abdul <abdul.cool987@gmail.com>
2021-09-25 05:43:27 -0700

    This is my first commit

---
+++ src/test/java/qaclcikacademy/RestAPiTest.java
@@ -0,0 +1,23 @@
+package qaclcikacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPiTest {
+	
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+
+	}
+
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+
+	}
+
+}
